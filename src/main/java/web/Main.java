4be3e343948daff0ef77b695a0fdbd3566a087f0
Noobowski <p.a.kons@yandex.ru>
2024-06-30 22:41:36 +0300

    Перенёс hiber-e.properties из старого проекта

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -2,6 +2,9 @@ package web;
 
 
 import org.springframework.context.annotation.Bean;
+import web.model.User;
+import web.service.UserService;
+import web.service.UserServiceImpl;
 
 import javax.sql.DataSource;
 
@@ -9,11 +12,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("At least it's working !");
 
-//        User user = new User("Pavel", "Konstantinov", (byte) 23);
-//        System.out.println(user);
+        User user = new User("Pavel", "Konstantinov", (byte) 23);
+        System.out.println(user);
 
-//        final UserService userService = new UserServiceImpl();
-//        System.out.println(userService.getUserById(3L));
+        final UserService userService = new UserServiceImpl();
+        System.out.println(userService.getUserById(2L));
 
     }
 
