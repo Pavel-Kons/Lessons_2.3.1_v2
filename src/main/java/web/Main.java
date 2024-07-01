4b50b97de263e8d47d5d9710443a2ba8c8d6518a
KonstantinovPA <p.a.kons@yandex.ru>
2024-07-01 09:53:03 +0300

    testing

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -11,6 +11,7 @@ import javax.sql.DataSource;
 public class Main {
     public static void main(String[] args) {
         System.out.println("At least it's working !");
+        System.out.println("At least it's working !");
 
         User user = new User("Pavel", "Konstantinov", (byte) 23);
         System.out.println(user);
